2f559e294a51737ccac394a6b6cb98ba65d0e394
Yudai IWAMIZU <yu.9824.job@gmail.com>
2021-04-27 14:11:21 +0900

    v0.1.1

--- kennard_stone/__init__.py
+++ kennard_stone/__init__.py
@@ -1,6 +1,6 @@
 from .kennard_stone import *
 
-__version__      = '0.1.0'
+__version__      = '0.1.1'
 __license__      = 'MIT'
 __author__       = 'yu-9824'
 __copyright__    = 'Copyright (c) 2021 yu-9824'
