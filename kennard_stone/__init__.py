56da68b967ff048f71b9addd3033b9b4e7a26e01
Yudai IWAMIZU <yu.9824.job@gmail.com>
2021-07-10 16:00:50 +0900

    Update to change username.

--- kennard_stone/__init__.py
+++ kennard_stone/__init__.py
@@ -1,16 +1,15 @@
 from .kennard_stone import *
 
-__version__      = '1.0.2'
+__version__      = '1.0.3'
 __license__      = 'MIT'
-__author__       = 'yu-9824'
-__copyright__    = 'Copyright (c) 2021 yu-9824'
+__author__       = 'yu9824'
+__copyright__    = 'Copyright (c) 2021 yu9824'
 __author_email__ = 'yu.9824.job@gmail.com'
-__user_id__ = 'yu-9824'
+__user_id__ = 'yu9824'
 __app_name__ = 'kennard_stone'
-__url__ = 'https://github.com/yu-9824/kennard_stone'
+__url__ = 'https://github.com/yu9824/kennard_stone'
 
 __all__ = [
     'KFold',
     'train_test_split',
-    '_KennardStone'
     ]
