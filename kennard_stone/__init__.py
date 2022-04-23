b07b82c9622f5d53dc482e2a854f939c7680b272
yu9824 <58211916+yu9824@users.noreply.github.com>
2022-04-23 17:46:05 +0900

    v1.1.0

--- kennard_stone/__init__.py
+++ kennard_stone/__init__.py
@@ -1,6 +1,6 @@
 from .kennard_stone import *
 
-__version__      = '1.0.4'
+__version__      = '1.1.0'
 __license__      = 'MIT'
 __author__       = 'yu9824'
 __copyright__    = 'Copyright © 2021 yu9824'
