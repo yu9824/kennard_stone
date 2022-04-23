0c1a099c7d403350f7aa8e73bbf67934299ce1ec
yu9824 <58211916+yu9824@users.noreply.github.com>
2022-04-23 17:45:49 +0900

    Update test.py

--- kennard_stone/test.py
+++ kennard_stone/test.py
@@ -1,5 +1,7 @@
-from kennard_stone import train_test_split
+from kennard_stone import train_test_split, KFold
 from sklearn.datasets import load_boston
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_validate
 import pandas as pd
 
 boston = load_boston()
@@ -8,3 +10,8 @@ y = pd.Series(boston.target, name = 'PRICE')
 
 X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.2)
 print(X_train, y_train, X_test, y_test)
+
+estimator = RandomForestRegressor(random_state=334, n_jobs=-1)
+
+for kf in (KFold(n_splits=5, alternate=True), KFold(n_splits=5, alternate=False)):
+    print(cross_validate(estimator, X, y, scoring='neg_mean_squared_error', n_jobs = -1, cv = kf, return_train_score=True))
