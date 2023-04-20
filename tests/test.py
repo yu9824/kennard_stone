23c68777817789db6358e6fade289e51c6aef6c3
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-04-21 01:10:12 +0900

    Move test file to tests directory

--- tests/test.py
+++ tests/test.py
@@ -13,19 +13,15 @@ if __name__ == "__main__":
     print(X_train, y_train, X_test, y_test)
 
     estimator = RandomForestRegressor(random_state=334, n_jobs=-1)
-
-    for kf in (
-        KFold(n_splits=5, alternate=True),
-        KFold(n_splits=5, alternate=False),
-    ):
-        print(
-            cross_validate(
-                estimator,
-                X,
-                y,
-                scoring="neg_mean_squared_error",
-                n_jobs=-1,
-                cv=kf,
-                return_train_score=True,
-            )
+    kf = KFold(n_splits=5)
+    print(
+        cross_validate(
+            estimator,
+            X,
+            y,
+            scoring="neg_mean_squared_error",
+            n_jobs=-1,
+            cv=kf,
+            return_train_score=True,
         )
+    )
