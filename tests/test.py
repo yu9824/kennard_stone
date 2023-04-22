759a807996e9691f55ecfe13e10bef501d804622
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-04-22 14:13:00 +0900

    Update test.py not to use pandas

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,19 @@
 if __name__ == "__main__":
-    from kennard_stone import train_test_split, KFold
     from sklearn.datasets import load_diabetes
     from sklearn.ensemble import RandomForestRegressor
     from sklearn.model_selection import cross_validate
-    import pandas as pd
 
-    diabetes = load_diabetes()
-    X = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
-    y = pd.Series(diabetes.target)
+    from kennard_stone import train_test_split, KFold
+
+    diabetes = load_diabetes(as_frame=True)
+    X = diabetes.data
+    y = diabetes.target
 
     X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.2)
     print(X_train, y_train, X_test, y_test)
 
     estimator = RandomForestRegressor(random_state=334, n_jobs=-1)
-    kf = KFold(n_splits=5)
+    kf = KFold(n_splits=5, shuffle=True)
     print(
         cross_validate(
             estimator,
