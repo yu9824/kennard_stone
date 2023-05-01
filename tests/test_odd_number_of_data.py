cba467ee76348a38428d8b6a3f0c314ab6bfbc85
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-05-01 18:26:11 +0900

    Create test for odd_number_of_data

---
+++ tests/test_odd_number_of_data.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_diabetes
+
+from kennard_stone.kennard_stone import _KennardStone
+
+
+def test_odd_number_of_data():
+    diabetes = load_diabetes(as_frame=True)
+
+    X = diabetes.data
+    _KennardStone(n_groups=2).get_indexes(X)
+
+    X = X.iloc[:-1]
+    _KennardStone(n_groups=2).get_indexes(X)
