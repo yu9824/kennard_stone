edd8a4fc668e19bbb77c122fcfabdcf68580feeb
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-05-01 15:54:41 +0900

    Prepare for large data test.

---
+++ tests/test_large_data.py
@@ -0,0 +1,18 @@
+# from sklearn.datasets import fetch_california_housing
+# import pytest
+
+# from kennard_stone import train_test_split
+
+
+# @pytest.fixture
+# def prepare_data():
+#     data = fetch_california_housing(as_frame=True)
+#     X = data.data
+#     y = data.target
+#     return (X, y)
+
+
+# def test_train_test_split_with_large(prepare_data):
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         *prepare_data, test_size=0.2
+#     )
