e199983160cdb7e5712042e5ed0fea3d9e7f824d
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-12-04 00:32:53 +0900

    Update setup.cfg

--- kennard_stone/__init__.py
+++ kennard_stone/__init__.py
@@ -11,9 +11,6 @@ __version__ = "2.2.0dev0"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2021 yu9824"
-__author_email__ = "yu.9824.job@gmail.com"
-__user_id__ = "yu9824"
-__app_name__ = "kennard_stone"
 __url__ = "https://github.com/yu9824/kennard_stone"
 
 __all__ = [
