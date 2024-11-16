6a55126b58b829134a227b8ce4d51154b5d10f68
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-11-16 15:01:13 +0900

    feat: Create _type_alias.py

---
+++ kennard_stone/_type_alias.py
@@ -0,0 +1,37 @@
+from typing import Literal, TypeAlias, Union
+
+from .utils import is_installed
+
+if is_installed("torch"):
+    import torch  # type: ignore
+
+    Device: TypeAlias = Union[
+        "torch.device", str, Literal["cpu", "cuda", "mps"]
+    ]
+else:
+    Device: TypeAlias = Union[str, Literal["cpu"]]
+
+Metrics: TypeAlias = Literal[
+    "cityblock",
+    "cosine",
+    "euclidean",
+    "l1",
+    "l2",
+    "manhattan",
+    "braycurtis",
+    "canberra",
+    "chebyshev",
+    "correlation",
+    "dice",
+    "hamming",
+    "jaccard",
+    "mahalanobis",
+    "minkowski",
+    "rogerstanimoto",
+    "russellrao",
+    "seuclidean",
+    "sokalmichener",
+    "sokalsneath",
+    "sqeuclidean",
+    "yule",
+]
