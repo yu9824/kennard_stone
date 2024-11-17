8ac4b465446152acf2ca7483d3fb7f46b1d7ed4d
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-11-18 00:46:11 +0900

    test: Update optional-dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,9 @@ from sklearn.datasets import fetch_california_housing, load_diabetes
 
 @pytest.fixture(scope="session")
 def prepare_data() -> tuple[np.ndarray, np.ndarray]:
-    return load_diabetes(return_X_y=True)
+    return load_diabetes(return_X_y=True, as_frame=True)
 
 
 @pytest.fixture(scope="session")
 def prepare_large_data() -> tuple[np.ndarray, np.ndarray]:
-    return fetch_california_housing(return_X_y=True)
+    return fetch_california_housing(return_X_y=True, as_frame=True)
