c8902e98740e39ad5df06f571b61e89d2a3aaf15
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-11-18 01:05:35 +0900

    test: Update test to use gpu

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from logging import DEBUG
+
 import numpy as np
 import pytest
 from sklearn.datasets import fetch_california_housing, load_diabetes
+
+from kennard_stone.logging import get_root_logger
 
 
 @pytest.fixture(scope="session")
@@ -13,3 +17,8 @@ def prepare_data() -> tuple[np.ndarray, np.ndarray]:
 @pytest.fixture(scope="session")
 def prepare_large_data() -> tuple[np.ndarray, np.ndarray]:
     return fetch_california_housing(return_X_y=True, as_frame=True)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_logger():
+    get_root_logger().setLevel(DEBUG)
