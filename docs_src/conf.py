93c870edf569c519a4215bf6e17da9de6cae2e20
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-11-18 00:16:33 +0900

    docs: Create sphinx source

---
+++ docs_src/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from kennard_stone import __author__, __version__
+
+project = "kennard-stone"
+copyright = "2024, yu9824"
+author = __author__
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",  # docstringからドキュメントを作成してくれる。
+    "sphinx.ext.napoleon",  # google式・numpy式docstringを整形してくれる。
+    "sphinx.ext.githubpages",  # github-pages用のファイルを生成してくれる。
+    "recommonmark",  # markdownで書けるようにする。
+    "sphinx_markdown_tables",  # markdownの表を書けるようにする。
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# for markdown documentations
+source_suffix = [".rst", ".md"]
+source_parsers = {
+    ".md": "recommonmark.parser.CommonMarkParser",
+}
+
+# for multi-version
+# smv_tag_whitelist = r"^v\d+\.\d+.\d+$"  # Include tags like "v2.1.1"
+# smv_branch_whitelist = r"^(main).*$"  # Include 'main' branch only
---
+++ docs_src/index.rst
@@ -0,0 +1,21 @@
+.. kennard-stone documentation master file, created by
+   sphinx-quickstart on Mon May  6 11:54:28 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to kennard-stone's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
