72aacea2792362b0dc3c13203033c3a80772c096
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-11-18 23:01:30 +0900

    v3.0.0

--- src/kennard_stone/__init__.py
+++ src/kennard_stone/__init__.py
@@ -7,13 +7,13 @@ Copyright © 2021 yu9824
 
 from ._core._core import KFold, train_test_split
 
-__version__ = "3.0.0rc1"
+__version__ = "3.0.0"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2021 yu9824"
 __url__ = "https://github.com/yu9824/kennard_stone"
 
-__all__ = [
+__all__ = (
     "KFold",
     "train_test_split",
-]
+)
