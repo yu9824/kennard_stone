63ca4397830bebc6594abd5e95fae39cf022ab9b
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-08-28 22:22:58 +0900

    v3.0.1

--- src/kennard_stone/__init__.py
+++ src/kennard_stone/__init__.py
@@ -28,7 +28,7 @@ Copyright © 2021 yu9824
 
 from ._core._core import KFold, train_test_split
 
-__version__ = "3.0.1-rc.1"
+__version__ = "3.0.1"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2021 yu9824"
